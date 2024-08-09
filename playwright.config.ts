99f36e5e5200ad33e7349b64d75b25b757b5ca8b
Tóth Zsolt Dániel <168081171+tothdanieldamit@users.noreply.github.com>
2024-08-09 19:32:26 +0200

    make local https optional for dev via script

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig, devices} from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 import * as process from 'node:process';
 
 /**
@@ -6,50 +6,50 @@ import * as process from 'node:process';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	forbidOnly: !!process.env.CI,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
 
-	/* Run tests in files in parallel */
-	fullyParallel: true,
+  /* Run tests in files in parallel */
+  fullyParallel: true,
 
-	/* Configure projects for major browsers */
-	projects: [
-		{
-			name: 'chromium',
-			use: {
-				...devices['Desktop Chrome'],
-				viewport: { height: 1080, width: 1920 },
-			},
-		},
-	],
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { height: 1080, width: 1920 },
+      },
+    },
+  ],
 
-	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: [process.env.CI ? ['github'] : ['list']] 
-	
-	/* Retry on CI only */,
-	retries: process.env.CI ? 1 : 0,
-	testDir: './tests/e2e',
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [process.env.CI ? ['github'] : ['list']],
 
-	/* Maximum timeout for each test in ms */
-	timeout: 30 * 1000,
+  /* Retry on CI only */
+  retries: process.env.CI ? 1 : 0,
+  testDir: './tests/e2e',
 
-	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-	use: {
-		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'https://localhost:3000/en',
+  /* Maximum timeout for each test in ms */
+  timeout: 30 * 1000,
 
-		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: 'on-first-retry',
-	},
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://localhost:3000/en',
 
-	/* Run your local test dev server before starting the tests */
-	webServer: {
-		command: 'pnpm start:dev',
-		reuseExistingServer: !process.env.CI,
-		timeout: 30 * 1000,
-		url: 'https://localhost:3000/en',
-	},
-	
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Run your local test dev server before starting the tests */
+  webServer: {
+    command: 'pnpm start:dev',
+    reuseExistingServer: !process.env.CI,
+    timeout: 30 * 1000,
+    url: 'http://localhost:3000/en',
+  },
+
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
 });
